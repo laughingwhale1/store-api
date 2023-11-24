a466f0fbdf4365594238f0de064029039b0934b2
rodion <luckypalm95@gmail.com>
2023-11-24 09:02:45 +0200

    add auth controller and admin middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\AuthLoginRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(AuthLoginRequest $authLoginRequest) {
+        $remember = $authLoginRequest['remember'] ?? false;
+        unset($authLoginRequest['remember']);
+
+        if(!Auth::attempt($authLoginRequest->validated(), $remember)) {
+            return response([
+                'message' => 'Email or password is incorrect'
+            ], 422);
+        }
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        if (!$user->is_admin) {
+            Auth::logout();
+
+            return response([
+                'message' => 'You dont have permission to authenticate as admin'
+            ], 403);
+        }
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+
+    public function logout() {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+
+        return response('', 204);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,4 +12,12 @@ Route::prefix('users')->group(function () {
     Route::post('', [UserController::class, 'createUser']);
     Route::put('{id}', [UserController::class, 'updateUser']);
     Route::delete('{id}', [UserController::class, 'deleteUser']);
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
 });
